2f9aa771ec3084e9d4128345a8bb7ccd79f59b95
EXisTAnZ <ozim_ssspam@mail.ru>
2023-07-08 12:04:42 +0300

    feat: add ws server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import { httpServer } from './http_server/';
+import { WebSocketServer } from 'ws';
+import dotenv from 'dotenv';
 
-const HTTP_PORT = 8181;
+dotenv.config();
+
+const HTTP_PORT = process.env.HTTP_PORT || 8181;
+const WS_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: WS_PORT });
+console.log(`Websocket server starting on ${WS_PORT} port!`);
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send(JSON.stringify({
+    type: "create_room",
+    data: "",
+    id: 0,
+  }));
+});
